101e2d963898695ead54416cab20e3225570bebb
Sibuls <sebo.orlowski@gmail.com>
2020-05-20 17:34:55 +0200

    Updates

---
+++ precache-manifest.947d8b362d477a83edaaeefd1ba28556.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ce5daa052aa3cf3355",
+    "url": "/react.app/static/css/main.51ce8c81.chunk.css"
+  },
+  {
+    "revision": "f1ce5daa052aa3cf3355",
+    "url": "/react.app/static/js/main.f1ce5daa.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/react.app/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "4cee9b87783b6b9d6b90",
+    "url": "/react.app/static/js/runtime~main.4cee9b87.js"
+  },
+  {
+    "revision": "f489966f26b2bed31d4397f57cf86102",
+    "url": "/react.app/index.html"
+  }
+];
